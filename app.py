341fbdd595bc07d55bb51da42d4636ad07b0d2a2
CikhungTerapiTinggiBadan <ziadan2005@gmail.com>
2025-10-20 14:09:02 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -22,12 +22,12 @@ luas rumah, jumlah kamar tidur, dan jumlah kamar mandi.
 col1, col2 = st.columns(2)
 
 with col1:
-    sqft_living = st.number_input("Luas Rumah (sqft)", min_value=100, max_value=10000, step=50, value=1800)
-
+    sqft_living = st.number_input("Jumlah Kamar Mandi", min_value=1.0, max_value=8.0, step=0.25, value=2.0
+)
 with col2:
     bedrooms = st.number_input("Jumlah Kamar Tidur", min_value=1, max_value=10, step=1, value=3)
 
-bathrooms = st.slider("Jumlah Kamar Mandi", min_value=1.0, max_value=8.0, step=0.25, value=2.0)
+bathrooms = st.slider("Luas Rumah (sqft)", min_value=100, max_value=10000, step=50, value=1800)
 
 # =============================
 # Tombol Prediksi
